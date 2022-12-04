4028ce742a5e3fe1f7fdb57b9f65d633a0b8effc
Jacek Kosy <jacek.kosy@outlook.de>
2022-12-04 21:31:47 +0100

    added first node

--- asi-project/src/asi_project/pipelines/sprint_two_housing/nodes.py
+++ asi-project/src/asi_project/pipelines/sprint_two_housing/nodes.py
@@ -1,4 +1,18 @@
 """
-This is a boilerplate pipeline 'sprint_two_insurance'
+This is a boilerplate pipeline 'sprint_two_housing'
 generated using Kedro 0.18.3
 """
+import numpy as np
+import pandas as pd
+
+def add_synthetic_features(housing_raw_data: pd.DataFrame) -> pd.DataFrame:
+    housing = housing_raw_data
+    housing["income_cat"] = pd.cut(housing["median_income"],
+                                   bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                                   labels=[1, 2, 3, 4, 5])
+
+    housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
+    housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
+    housing["population_per_household"] = housing["population"] / housing["households"]
+    return housing
+
--- asi-project/src/asi_project/pipelines/sprint_two_housing/pipeline.py
+++ asi-project/src/asi_project/pipelines/sprint_two_housing/pipeline.py
@@ -4,7 +4,13 @@ generated using Kedro 0.18.3
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import add_synthetic_features
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            add_synthetic_features,
+            inputs="housing_data_raw",
+            outputs="housing_data_with_new_features"),
+    ])
